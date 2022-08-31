9af24a492396973dd7cfa3a620f80d70180a6b4a
secbaymax <secbaymax@gmail.com>
2022-08-31 13:40:18 +0000

    Network Update

--- main.tf
+++ main.tf
@@ -49,8 +49,8 @@ resource "azurerm_network_security_group" "nsg" {
     protocol               = "Tcp"
     source_port_range      = "*"
     destination_port_range = "3389"
-    #   source_address_prefix      = "10.0.0.0/16"
-    source_address_prefix      = "*"
+    source_address_prefix  = "10.0.0.0/16"
+    #source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
